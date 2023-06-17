818bb8037270f3c83ea4ec93b161b8a55c00bb5c
Vishal Thukral <vishal@mymindand.me>
2023-06-17 15:58:44 -0400

    basic working demo

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-# hello world streamlit app
 import streamlit as st
+import requests
 
+# Heading block
+st.title("Your Streamlit App")
 
-st.title('Hello World')
+# Description block
+st.markdown("""
+This is a simple Streamlit app that takes user input and makes an API call when a button is clicked.
+""")
 
-st.write('This is my first app')
+# Text input
+rfla = st.text_input("Please entre PDB identifier(s) space delimited:")
+
+# Button
+if st.button('Call API'):
+    # Make the API call
+    # NOTE: Replace 'http://api.example.com' with your actual API endpoint
+    response = requests.get(f'http://35.194.49.193/pdbrenum?rfla={rfla}h&mmCIF=true', stream=True)
+    # response contains file
+    # download file
+
+    file_data = b""
+    for chunk in response.iter_content(chunk_size=8192):
+        if chunk:  # filter out keep-alive new chunks
+            file_data += chunk
+
+    # Provide a download button for the file
+    st.download_button(
+        label="Download the file",
+        data=file_data,
+        file_name="response.zip",
+        mime="application/zip"
+    )
