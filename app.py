f51f69f1e9ac4708dc30a669663ab169a637ad32
Vishal Thukral <vishal@mymindand.me>
2023-06-17 16:38:27 -0400

    code archive

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ import streamlit as st
 import requests
 
 # Heading block
-st.title("Your Streamlit App")
+st.title("PDBrenum")
 
 # Description block
 st.markdown("""
-This is a simple Streamlit app that takes user input and makes an API call when a button is clicked.
+PDBrenum provides PDB files in mmCIF and legacy-PDB format in which residues in the coordinates (and all other fields) are renumbered according to their UniProt sequences.
 """)
 
 # Text input
 rfla = st.text_input("Please entre PDB identifier(s) space delimited:")
 
 # Button
-if st.button('Call API'):
+if st.button('Generate Renumebred PDB'):
     # Make the API call
     # NOTE: Replace 'http://api.example.com' with your actual API endpoint
     response = requests.get(f'http://35.194.49.193/pdbrenum?rfla={rfla}h&mmCIF=true', stream=True)
@@ -27,7 +27,7 @@ if st.button('Call API'):
 
     # Provide a download button for the file
     st.download_button(
-        label="Download the file",
+        label="Download the files",
         data=file_data,
         file_name="response.zip",
         mime="application/zip"
